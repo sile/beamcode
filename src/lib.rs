728542cfc976a4146c6a73a826721d52211b09d1
Takeru Ohta <phjgt308@gmail.com>
2022-05-09 00:44:06 +0900

    Add label op decode

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,20 @@ pub enum ParseError {
     #[error("supported instruction set version is {INSTRUCTION_SET_VERSION}, but got {version}")]
     UnsupportedInstructionSetVersion { version: u32 },
 
+    #[error("expected a {expected:?} arg, but got {actual:?}")]
+    UnexpectedArg {
+        expected: &'static str,
+        actual: CompactTerm,
+    },
+
     #[error(transparent)]
     IoError(#[from] std::io::Error),
+}
+
+impl ParseError {
+    fn unexpected_arg(expected: &'static str, actual: CompactTerm) -> Self {
+        Self::UnexpectedArg { expected, actual }
+    }
 }
 
 pub fn parse_code_chunk(chunk: &CodeChunk) -> Result<(), ParseError> {
@@ -27,34 +39,100 @@ pub fn parse_code_chunk(chunk: &CodeChunk) -> Result<(), ParseError> {
     dbg!(chunk.bytecode.len());
     let mut reader = &mut &chunk.bytecode[..];
     while !reader.is_empty() {
-        parse_op(&mut reader)?;
+        let op = Op::decode(&mut reader)?;
+        dbg!(op);
     }
     todo!()
-}
-
-pub fn parse_op<R: Read>(reader: &mut R) -> Result<(), ParseError> {
-    let opcode = reader.read_u8()?;
-    todo!("OP: {opcode}")
 }
 
 // https://blog.stenmans.org/theBeamBook/#SEC-BeamModulesCTE
 pub fn decode_compact_term() {}
 
+pub type DecodeError = ParseError;
+
+#[derive(Debug, Clone)]
+pub enum CompactTerm {
+    Literal(Literal),
+    Todo,
+}
+
+impl CompactTerm {
+    pub fn decode<R: Read>(reader: &mut R) -> Result<Self, DecodeError> {
+        let tag = reader.read_u8()?;
+        match tag & 0b111 {
+            0b000 => {
+                let index = (tag >> 4) as usize;
+                Ok(Self::Literal(Literal { index }))
+            }
+            0b001 => {
+                todo!();
+            }
+            0b010 => {
+                todo!();
+            }
+            0b011 => {
+                todo!();
+            }
+            0b100 => {
+                todo!();
+            }
+            0b101 => {
+                todo!()
+            }
+            0b110 => {
+                todo!();
+            }
+            _ => {
+                todo!();
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct Literal {
+    pub index: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct LabelOp {
+    pub literal: Literal,
+}
+
+impl LabelOp {
+    pub const CODE: u8 = 1;
+    pub const ARITY: usize = 1;
+
+    pub fn decode_args<R: Read>(reader: &mut R) -> Result<Self, DecodeError> {
+        match CompactTerm::decode(reader)? {
+            CompactTerm::Literal(literal) => Ok(Self { literal }),
+            term => Err(DecodeError::unexpected_arg("literal", term)),
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum Op {
-    Label { label: String },
+    Label(LabelOp),
 }
 
 impl Op {
+    pub fn decode<R: Read>(reader: &mut R) -> Result<Self, DecodeError> {
+        match reader.read_u8()? {
+            LabelOp::CODE => LabelOp::decode_args(reader).map(Self::Label),
+            op => todo!("{op}"),
+        }
+    }
+
     pub fn opcode(&self) -> u8 {
         match self {
-            Self::Label { .. } => 1,
+            Self::Label { .. } => LabelOp::CODE,
         }
     }
 
     pub fn arity(&self) -> usize {
         match self {
-            Self::Label { .. } => 1,
+            Self::Label { .. } => LabelOp::ARITY,
         }
     }
 }
